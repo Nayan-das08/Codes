a5c80a471d3c47d5ab722ac44e5c59aa7475e9a1
Nayan <nayanpcstudy@gmail.com>
2022-10-30 03:25:48 +0530

    [M] tsp_b.py

--- ADA/tsp_b.c
+++ ADA/tsp_b.c
@@ -3,11 +3,22 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-int n;
+int n = 5+1;
+int size = 10;
+
+
 
 int main()
 {
-	printf("Enter number of nodes : ");
-	scanf("")
+	float z = INFINITY;
+	float W[size][size] = {
+		{0,  1,  2,  3,  4,  5},
+		{1,  z, 20, 30, 10, 11},
+		{2, 15,  z, 18,  4,  2},
+		{3,  3,  5,  z,  2,  4},
+		{4, 19,  6, 18,  z,  3},
+		{5, 16,  4,  7, 16, z}
+	};
+
 	return 0;
 }
