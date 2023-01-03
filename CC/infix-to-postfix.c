ec7e590744f4aec8bb5877894f3a994706909d64
NayanTab <nayanpcstudy@gmail.com>
2023-01-03 23:37:54 +0530

    [M] infix-to-prostfix.c

--- CC/infix-to-postfix.c
+++ CC/infix-to-postfix.c
@@ -1,3 +1,4 @@
+// convert infix expression to postfix expression
 # include <stdio.h>
 # include <string.h>
 
@@ -11,10 +12,12 @@ int check(char c)
         return -1;
 }
 
-void separate(char infix[], char new[][10])
+int separate(char infix[], char new[][10])
 {
     char temp[10];
     int k=0, count=0;
+    strcpy(new[count++], "(");
+
     for (int i=0; i<strlen(infix); i++)
     {
         if (check(infix[i]) == 1)
@@ -23,38 +26,102 @@ void separate(char infix[], char new[][10])
         }
         else if (infix[i] == '+' || infix[i] == '-' || infix[i] == '*' || infix[i] ==  '/')
         {
-            //printf("%c is operator\n", infix[i]);
             strcpy(new[count], temp);
             count++;
             strcpy(new[count], (char [2]) {infix[i], '\0'});
-            //new[count][0] = infix[i];
-            //strncat(new[count],"\0",1);
             count++;
             strcpy(temp,"");
         }
-        printf("\n%c \ttemp = %s \tcount = %d",infix[i],temp,count);
             
     }
     
     strcpy(new[count], temp);
     count++;
+    strcpy(new[count], ")");
     printf("\n\n");
-    for (int i=0; i<count; i++)
+    for (int i=0; i<=count; i++)
     {
         printf("%s\n", new[i]);
     }
+    return count;
+}
 
-    printf("\n\ncount = %d", count);
+int precedence(char c)
+{
+    if (c == '-')
+        return 1;
+    else if (c == '+')
+        return 2;
+    else if (c == '/')
+        return 3;
+    else if (c == '*')
+        return 4;
+    return 0;
+}
+
+void convert(char infix[][10], int count)
+{
+    int top=0;
+    char stack[10][5], a, b;
+    for (int i=0; i<=count; i++)
+    {
+        if (strcmp(infix[i],"(") == 0) 
+            strcpy(stack[top],infix[i]);
+
+        // numeric
+        else if (check(infix[i][0]) == 1)
+            printf("%s ", infix[i]);
+        
+        // operator
+        else if (check(infix[i][0]) == 0)
+        {
+            if (check(stack[top][0]) == 0)
+            {
+                a = infix[i][0];
+                b = stack[top][0];
+                // top has higher precedence
+                if (precedence(b) >= precedence(a))
+                {
+                    // pop
+                    printf("%s ", stack[top]);
+                    top--;
+                    // push
+                    strcpy(stack[top],infix[i]);
+                }
+                else 
+                    strcpy(stack[top],infix[i]);
+            }
+        }
+
+        else if (strcmp(infix[i],")") == 0)
+        {
+            while (strcmp(stack[top], "(") != 0)
+            {
+                // pop
+                printf("%s ", stack[top]);
+                top--;
+            }
+            
+        }
+
+        printf("%s \t", infix[i]);
+        //for (int k=0; k<=top; k++)
+          //  printf("%s", stack[top]);
+        printf("\n"); 
+    }
 }
 
 int main()
 {
-    char infix[10], new[10][10];
+    char infix[10], infix_sep[10][10];
+    int count;
+
     printf("Enter infix expression : ");
     scanf("%s",infix);
     printf("infix = %s\n", infix);
     
-    separate(infix,new);
+    count = separate(infix,infix_sep);
+    convert(infix_sep, count);
 
     printf("\n");
     return 0;
