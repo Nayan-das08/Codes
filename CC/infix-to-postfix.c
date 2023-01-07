3655fe2c5fadc13731a541ec6ab11082c1e3895a
NayanTab <nayanpcstudy@gmail.com>
2023-01-07 14:21:46 +0530

    [M] infix-to-postfix.c

--- CC/infix-to-postfix.c
+++ CC/infix-to-postfix.c
@@ -24,15 +24,20 @@ int separate(char infix[], char new[][10])
     {
         if (check(infix[i]) == 1)
         {
-            strncat(temp, &infix[i], 1); 
+            strncat(temp, &infix[i], 1);
         }
-        else 
+        else if (check(infix[i]) == 2)
         {
             strcpy(new[count], temp);
             count++;
             strcpy(new[count], (char [2]) {infix[i], '\0'});
             count++;
             strcpy(temp,"");
+        }
+        else if (check(infix[i]) == 0)
+        {
+        	strcpy(new[count], (char [2]) {infix[i], '\0'});
+        	count++;
         }
     }
     
@@ -63,8 +68,8 @@ int precedence(char c)
 void convert(char infix[][10], int count)
 {
     int top=-1;
-    char stack[10][5], output[20][10], j=0, a, b;
-
+    char stack[10][5], output[20][10], a, b;
+		int j=-1;
     for (int i=0; i<=count; i++)
     {
     	if (strcmp(infix[i], "(") == 0)
@@ -74,17 +79,20 @@ void convert(char infix[][10], int count)
     	else if (check(infix[i][0]) == 1)
     	{
     		printf("%s ", infix[i]);
+    		// strcpy(output[++j], infix[i]);
     	}
     	else if (check(infix[i][0]) == 2)
     	{
     		a = infix[i][0];
     		b = stack[top][0];
     		if (check(b) == 2)
+    		// stack top is an operator
     		{
     			if (precedence(b) > precedence(a))
     			{
     				// pop
     				printf("%s ", stack[top]);
+    				// strcpy(output[++j], infix[i]);
     				// push
     				strcpy(stack[top], infix[i]);
     			}
@@ -106,9 +114,19 @@ void convert(char infix[][10], int count)
     		{
     			// pop
     			printf("%s ", stack[top]);
+    			// strcpy(output[++j], stack[top]);
     			top--;
     		}
+    		if (stack[top][0] == '(')
+    			top--;
     	}
+
+   //  	printf("\n%s \tstack=", infix[i]);
+   //  	for (int k=0; k<=top; k++)
+   //  		printf("%s ", stack[k]);
+			// printf("\to/p=");
+			// for (int k=0; k<=j; k++)
+			// 	printf("%s ", output[k]);
     }
 
 }
@@ -124,6 +142,9 @@ int main()
     
     printf("\ninfix   = \t");
     count = separate(infix,infix_sep);
+    // printf("\n");
+    // for (int i=0; i<=count; i++)
+    	// printf("%s\n", infix_sep[i]);
     printf("\npostfix = \t");
     convert(infix_sep, count);
 
